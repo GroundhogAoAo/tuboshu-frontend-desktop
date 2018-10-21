fe7e0577019c99edf25e679936ed69657b0991ed
wk <>
2018-10-21 20:41:54 +0800

    .

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <el-button @click="message" type="success" icon="el-icon-search" round>默认按钮</el-button>
     <router-view></router-view>
   </div>
 </template>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,13 +5,14 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-import iView from 'iview'
-import 'iview/dist/styles/iview.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 
 
-Vue.use(iView)
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
