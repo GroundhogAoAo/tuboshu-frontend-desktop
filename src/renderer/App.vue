36eb387b070dc0f6897c8e1d9e404eb2d3de9404
wk <>
2018-10-21 14:56:37 +0800

    [update] init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'tuboshu-frontend-desktop'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
